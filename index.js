478fdcbf2dbd1a59c5a89c03eac88740ec97bea8
Mohamed Fazil <nmohammedfazil790@gmail.com>
2024-06-07 20:22:34 +0530

    uploaded

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import fs from 'fs';
 import pg from 'pg';
-import { log } from 'console';
-
+import multer from 'multer';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 const db = new pg.Client({
-    user:"postgres",
-    host:"localhost",
-    password:"0000",
-    database:"imageTest",
-    port:5432,
+    user: "postgres",
+    host: "localhost",
+    password: "shin2005-89",
+    database: "imageTest",
+    port: 5432,
 
 })
 
 db.connect();
+const upload = multer({ dest: "uploads/"})
 
 const result = await db.query("select * from images")
 console.log(result.rows);
 
 
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
 
     res.render("index.ejs")
     //this change after edit and second commit
@@ -35,13 +35,19 @@ app.get("/", (req, res) =>{
 
 
 
-app.post("/image", (req, res) =>{
-    const imageURL = req.body.images;
-    console.log(imageURL);
-    res.send(imageURL)
-   let data =  fs.readFileSync(imageURL);
-   console.log("data"+data);
-   console.log("file edit");
+app.post("/image", upload.single('images'), async (req, res) => {
+    const imageURL = req.file.path;
+    try{
+        const ImageData = fs.readFileSync(imageURL);
+        console.log(ImageData)
+        db.query("INSERT INTO images (data, name) VALUES ($1, $2)", [ImageData, "NAME"])
+        console.log("IMAGE UPLOADED SUCCESSFULLY....");
+        
+
+
+    }catch(err){
+        console.log("Error WHILE UPLOADING.........");
+    }
 
    
 
@@ -49,6 +55,6 @@ app.post("/image", (req, res) =>{
 })
 
 
-app.listen(4000, () =>{
+app.listen(4000, () => {
     console.log("Server port 4000");
 })
